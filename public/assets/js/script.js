ad6b76151ee00796e5a3d223e52b73e084528b9e
Emma Petreski <emma-petreski@hotmail.fr>
2020-09-28 14:55:42 +0200

    cookie update

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -10,33 +10,82 @@ window.onload = function () {
     //////////////////////////////// ACCUEIL /////////////////////////////
     //COOKIES MOBILE
     //apparition
-    var cookiesMobile = document.getElementById('cookiesMobile');
+    //////////////////////////////// ACCUEIL /////////////////////////////
 
-    function timerMob() {
-        cookiesMobile.style.display = 'block';
-    }
-    setTimeout(timerMob, 1000);
-
-    //disparition
-    $('#accepteMob').click(function () {
-        $('#cookiesMobile').hide();
-    })
-
-    $('#croix').click(function () {
-        $('#cookiesMobile').hide();
-    })
-
-    //COOKIES DESKTOP
-    //apparition
+    //COOKIES DESKTOP + MOBILE
     var cookiesConteneur = document.getElementById('cookiesConteneur');
+    var accepte = document.getElementById('accepte');
+    var cookiesMobile = document.getElementById('cookiesMobile');
+    var accepteMob = document.getElementById('accepteMob');
 
-    function timer() {
-        cookiesConteneur.style.display = 'block';
-    } setTimeout(timer, 1000);
+    //CLICK DESKTOP = appeler les fonctions + paramètres du cookie + ne pas afficher mobile
+    accepte.addEventListener('click', (function() {
+        createCookie('cookiki', 'en place', 0);
+        readCookie(name);
+        eraseCookie(name);
+        cookiesConteneur.style.display = 'none';
+        cookiesMobile.style.display = 'none';
+    }))
 
-    //disparition
-    $('#accepte').click(function () {
-        $('#cookiesConteneur').hide();
+    // CLICK MOBILE = appeler les fonctions + paramètres du cookie + ne pas afficher desktop
+    accepteMob.addEventListener('click', (function(){
+        createCookie('cookiki', 'en place', 0);
+        readCookie(name);
+        eraseCookie(name);
+        cookiesMobile.style.display = 'none';
+        cookiesConteneur.style.display = 'none';
+    }))
+
+    //CREER FONCTION COOKIE
+    function createCookie(name, value, days) {
+        var expires;
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toGMTString();
+        } else {
+            expires = "-1";
+        }
+        //place cookie dans document.cookie
+        document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+    }
+
+    //LIT LE COOKIE, LE MET EN TABLEAU, VERIFIE LE NOM SANS ESPACE ET LE SAUVEGARDE POUR CHECK
+    function readCookie(name) {
+        var nameEQ = encodeURIComponent(name) + "=";
+        var tabcookie = document.cookie.split(';');
+        for (var i = 0; i < tabcookie.length; i++) {
+            var c = tabcookie[i];
+            while (c.charAt(0) === ' ')
+                c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0)
+                return decodeURIComponent(c.substring(nameEQ.length, c.length));
+        }
+        return null;
+    }
+
+    //CHECK SI COOKIE EST LA (n'affiche pas div) OU PAS (affiche div)
+    function checkCookie() {
+        if (readCookie('cookiki') != null) {
+            cookiesConteneur.style.display = 'none';
+            cookiesMobile.style.display = 'none';
+        } else {
+            cookiesConteneur.style.display = 'block';
+            cookiesMobile.style.display = 'block';
+        }
+    }
+
+    //LANCER LA FONCTION CHECK
+    checkCookie();
+
+    //CREATE COOKIE -1 POUR QU'IL SOIT EXPIRE ET QU'IL S'EFFACE
+    function eraseCookie(name) {
+        createCookie(name, "", -1);
+    };
+
+    //CROIX EN MOBILE
+    $('#croix').click(function(){
+        $('#cookiesMobile').hide();
     })
 
     //TEXTE INSCRIPTION COLLAPSE MOBILE
