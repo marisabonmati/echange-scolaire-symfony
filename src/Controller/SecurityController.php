7b08881d62e2293c0d383f6287bfa25dff4c4fa5
Emma Petreski <emma-petreski@hotmail.fr>
2020-10-01 11:06:28 +0200

    mdp

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -28,7 +28,11 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        //throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        $this->get('session')->getFlashBag()->add(
+            'notice',
+            'Customer Added!'
+        );
 
     }
 }
--- src/Form/ProfilType.php
+++ src/Form/ProfilType.php
@@ -46,17 +46,6 @@ class ProfilType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Renseignez votre mot de passe',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Votre mot de passe doit contenir au moins 6 caractères',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
             ])
             ->add('lastName', TextType::class, ['label' => 'Nom : ', 'required' => true])
             ->add('firstname', TextType::class, ['label' => 'Prénom : '])
