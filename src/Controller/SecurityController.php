99ed41d867a14a729879799dbbb68176f23973aa
Emma Petreski <emma-petreski@hotmail.fr>
2020-10-01 11:21:25 +0200

    wip annonce

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -28,11 +28,6 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-        //throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-        $this->get('session')->getFlashBag()->add(
-            'notice',
-            'Customer Added!'
-        );
-
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
