a75c176545483136c762251a199234c4ce38e9f1
Emma Petreski <emma-petreski@hotmail.fr>
2020-10-01 16:22:33 +0200

    wip publi

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\Publication;
 use App\Entity\User;
+use App\Repository\PublicationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,10 +12,12 @@ class AnnonceController extends AbstractController
     /**
      * @Route("/annonce/{id}", name="annonce")
      */
-    public function index(User $user, Publication $publication)
+    public function index(User $user, PublicationRepository $publicationRepository)
     {
+        $publication = $publicationRepository->searchPublication();
         return $this->render('annonce/annonce.html.twig', [
             'user' => $user,
+            'list_publication' => $publicationRepository,
         ]);
     }
 }
